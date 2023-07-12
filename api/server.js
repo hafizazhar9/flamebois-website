f71d181db7368253c4ddd326aa41b8a66ab83c95
hafizazhar9 <90740898+hafizazhar9@users.noreply.github.com>
2023-07-13 07:08:03 +0800

    Update server.js

--- api/server.js
+++ api/server.js
@@ -11,7 +11,13 @@ const server = express()
 
 // Middleware
 server.use(bodyParser.json())
-server.use(cors({ credentials: true }))
+server.use(cors(
+  { 
+    origin: ["https:flamebois-websitem.vercel.app"],
+    methods: ["POST", "GET"]
+    credentials: true
+  }
+))
 server.use(authMiddleware.initialize)
 
 // Routes
